9c103545090e8228852f3b5ed27fda8929c63dcf
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-12-17 18:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const downloadimage = (image)=>{
+	return new Promise((resolve,reject)=>{
+		const img = new Image();
+	img.src=image.url;
+
+		img.onload =()=>resolve(img);
+		img.onerror=()=>reject(new Error(`failed to load image's URL ${image.url}`))
+	})
+}
+btn.addEventListener("click",()=>{
+	output.innerHTML='';
+
+	Promise.all(images.map(downloadimage)).then((imageelements)=>{
+		imageelements.forEach((img)=>{
+			output.appendChild(img);
+		})
+	}).catch((error)=>{
+		 output.innerHTML = `<p style="color: red;">${error.message}</p>`;
+	})
+})
